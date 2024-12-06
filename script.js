20ff42c4b62f4c681e187248e24de0c62aefd5b9
Dipwr <deopwr@gmail.com>
2024-12-06 23:19:34 +0100

    optimized collision solver

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ let toBeCleared = [];
 
 //timing setup
 let prevTs;
+let dtAcc = 0;
 
 //iniciate the program starting with the setup function
 setup();
@@ -44,19 +45,31 @@ function frame() {
 	let deltaTime = (now - prevTs) * simSpeed;
 	prevTs = now;
 
+    //diagnostics
+    
+
 	if(!paused){
+        timedEvents(deltaTime);
 		update(deltaTime);
-		draw();
+		draw(deltaTime);
 	}
 	//Recursive function (call itself)
 	requestAnimationFrame(frame);
 }
 
-function update(deltaTime){
-    solver.update(deltaTime);
+function timedEvents(deltaTime){
+    dtAcc += deltaTime;
+    if (dtAcc >= 0.01){
+        solver.objects.push(new VerletObject([580,400], 5, 1, [Math.random()*255,Math.random()*255,Math.random()*255], solver));
+        dtAcc = 0;
+    }
 }
 
-function draw(){
+function update(deltaTime){
+    solver.update(deltaTime, 4);
+}
+
+function draw(deltaTime){
     //clear prev frame
     for (let i = 0; i < toBeCleared.length; i++){
 		let base = (toBeCleared[i][0] + toBeCleared[i][1] * boundX) * 4;
@@ -77,6 +90,10 @@ function draw(){
 
     //draw frame
     ctx.putImageData(imageData, 0, 0);
+
+    ctx.font = "10px Arial";
+    ctx.fillText("FPS: " + (1/deltaTime).toFixed(2),10,10);
+    ctx.fillText("D.T.: " + (deltaTime*1000).toFixed(2) + "ms",10,20);
 }
 
 function putCircle(pos, r, color) {
@@ -107,20 +124,39 @@ function putPixel(x, y, color){
 }
 
 function Solver(){
-    this.objects = []
+    this.objects = [];
+
+    this.cells = [];
 
     this.gravity = [0, 1000];
 
-    this.update = function(deltaTime){
+    this.maxSize = 0;
+
+    this.update = function(deltaTime, subSteps){
         this.applyGravity();
         this.updatePositions(deltaTime);
-        this.solveCollisions()
-        this.applyConstraint([400,400], 200);
+        for(let i = 0; i < subSteps; i++){
+            this.solveCollisions()
+            this.applyConstraint([400,400], 200);
+        }
     }
 
     this.updatePositions = function(deltaTime){
+        //clear the cells
+        this.cells = [];
+	    for (let i = 0; i < Math.ceil(boundX/this.maxSize); i++){
+            this.cells.push([]);
+            for (let j = 0; j < Math.ceil(boundY/this.maxSize); j++){
+                this.cells[i].push([]);
+            }
+        }
+
         for (let i = 0; i < this.objects.length; i++){
             this.objects[i].updatePosition(deltaTime);
+
+            let cellx = Math.floor(this.objects[i].pos[0] / this.maxSize);
+		    let celly = Math.floor(this.objects[i].pos[1] / this.maxSize);
+		    this.cells[cellx][celly].push(i);
         }
     }
 
@@ -149,42 +185,83 @@ function Solver(){
     this.solveCollisions = function(){
         if (this.objects.length > 1){
             for (let i = 0; i < this.objects.length; i++){
-                for (let j = 0; j < this.objects.length; j++){
-                    if(i == j){continue;}
-                    const threshold = this.objects[i].size + this.objects[j].size;
+                let neighboors = this.getNeigbours(i);
+                for (let j = 0; j < neighboors.length; j++){
+                    const ni = neighboors[j];
+                    if(i == ni){continue;}
+                    const threshold = this.objects[i].size + this.objects[ni].size;
 
-                    dx = this.objects[i].pos[0] - this.objects[j].pos[0];
-                    dy = this.objects[i].pos[1] - this.objects[j].pos[1];
+                    dx = this.objects[i].pos[0] - this.objects[ni].pos[0];
+                    dy = this.objects[i].pos[1] - this.objects[ni].pos[1];
 
                     dist = Math.sqrt(dx*dx + dy*dy);
 
                     if (dist < threshold){
+                        const m1 = this.objects[i].mass;
+                        const m2 = this.objects[ni].mass;
+
                         const ndx = (dx / dist);
                         const ndy = (dy / dist);
-                        console.log(ndx * (threshold-dist) * (1/2),ndy * (threshold-dist) * (1/2));
-                        this.objects[i].pos[0] += ndx * (threshold-dist) * (1/2);
-                        this.objects[i].pos[1] += ndy * (threshold-dist) * (1/2);
+                        
+                        this.objects[i].pos[0] += ndx * (threshold-dist) * (1 - m1/(m1+m2));
+                        this.objects[i].pos[1] += ndy * (threshold-dist) * (1 - m1/(m1+m2));
 
-                        this.objects[j].pos[0] -= ndx * (threshold-dist) * (1/2);
-                        this.objects[j].pos[1] -= ndy * (threshold-dist) * (1/2);
+                        this.objects[ni].pos[0] -= ndx * (threshold-dist) * (1 - m2/(m1+m2));
+                        this.objects[ni].pos[1] -= ndy * (threshold-dist) * (1 - m2/(m1+m2));
                     }
                 }
             }
         }
     }
 
+    this.getNeigbours = function(particleIndex){
+        let cellx = Math.floor(this.objects[particleIndex].pos[0] / this.maxSize);
+        let celly = Math.floor(this.objects[particleIndex].pos[1] / this.maxSize);
+        
+        let PossibleNeighboors = [true,true,true,true,true,true,true,true,true];
+        if (cellx == 0){
+            PossibleNeighboors[0] = false;
+            PossibleNeighboors[3] = false;
+            PossibleNeighboors[6] = false;
+        }
+        if (cellx == Math.floor(boundX/this.maxSize)){
+            PossibleNeighboors[2] = false;
+            PossibleNeighboors[5] = false;
+            PossibleNeighboors[8] = false;
+        }
+        if (celly == 0){
+            PossibleNeighboors[0] = false;
+            PossibleNeighboors[1] = false;
+            PossibleNeighboors[2] = false;
+        }
+        if (celly == Math.floor(boundY/this.maxSize)){
+            PossibleNeighboors[6] = false;
+            PossibleNeighboors[7] = false;
+            PossibleNeighboors[8] = false;
+        }
+    
+        let neighboors = [];
+        for (let i = 0; i < 9; i++){
+            if (PossibleNeighboors[i] == false){continue;}
+            neighboors = neighboors.concat(this.cells[cellx+(-1+(i%3))][celly+(-1+(Math.floor(i/3)))]);
+        }
+        
+        return neighboors;
+    }
+
     this.setup = function(){
-        this.objects.push(new VerletObject([580,400], 10, [0,0,255]));
-        this.objects.push(new VerletObject([220,400], 20, [255,0,0]));
+        //this.objects.push(new VerletObject([220,400], 5, 10, [255,0,0], this));
     }
 }
 
-function VerletObject(pos, size, color){
+function VerletObject(pos, size, mass, color, solver){
     this.pos = pos;
     this.oldPos = pos;
     this.acceleration = [0,0];
 
     this.size = size;
+    this.mass = mass;
+
     this.color = color;
 
     this.updatePosition = function(deltaTime){
@@ -201,4 +278,12 @@ function VerletObject(pos, size, color){
         this.acceleration[0] += acc[0];
         this.acceleration[1] += acc[1];
     }
+
+    this.onConstruction = function(){
+        if (size > solver.maxSize*2){
+            solver.maxSize = size*2;
+        }
+    }
+
+    this.onConstruction();
 }
